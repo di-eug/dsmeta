e592ccb4fcb05edf0b02f9db5cc35942ca53ec9f
diovo <diogodeeugenio@gmail.com>
2022-07-15 21:24:33 -0300

    API test

---
+++ backend/src/main/java/com/devsup/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsup.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsup.dsmeta.entities.Sales;
+
+public interface SaleRepository extends JpaRepository <Sales, Long>{
+
+}
